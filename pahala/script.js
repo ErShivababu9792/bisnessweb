991c71fe67f071ff5acfd198697d64e40a4d9c4c
Shivanand vishwakarma <vshivanand469@gmail.com>
2025-01-03 00:26:42 +0530

    Add files via upload

---
+++ pahala/script.js
@@ -0,0 +1,33 @@
+// let main=document.querySelectorAll("main")
+// let crsr=document.getElementById("cursor");
+// main.addEventListener("mousemove",function(details){
+//   crsr.style.left=details.x +"px";
+//   crsr.style.top=details.y+"px";
+// })
+// let main=document.getElementById("main")
+// let crsr=document.getElementById("cursor");
+// main.addEventListener("mousemove",function(details){
+//   crsr.style.left=details.x +"px";
+//   crsr.style.top=details.y+"px";
+// })
+
+
+// let photo=document.getElementById("photo");
+// let arr=['<img src="./images/kkjkhjh.jpeg" alt="">']
+// let brr=['<img src="./images/new mst.jpeg" alt="">']
+// let btna=document.getElementById("btna")
+
+// x.addEventListener("click",function(){
+//   photo.innerHTML=arr;  
+// })
+// x.addEventListener("dblclick",function(){
+//     photo.innerText=brr;  
+//   })
+
+//   innertext
+let menu=document.getElementById("menu");
+let nav=document.getElementById("nav");
+ 
+menu.addEventListener("click",()=>{
+ menu.inner=nav;
+})
